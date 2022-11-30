ffa258ddf839499edbc5debf64860f1d95010b45
KronosGR <geo.elgeo@gmail.com>
2022-11-30 07:20:37 +0100

    stage 2/6 added simple server and client

--- JSON Database/task/src/client/Main.java
+++ JSON Database/task/src/client/Main.java
@@ -1,8 +1,29 @@
 package client;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 23456;
+
+    public static void main(String[] args) throws IOException {
+
+        Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+
+        System.out.println("Client started!");
+
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+
+        output.writeUTF("Give me a record # ");
+        output.writeUTF("12");
+
+        System.out.println("Sent: " + "Give me a record # 12");
+        String recMsg = input.readUTF();
+        System.out.println("Received: "+ recMsg);
     }
 }
--- JSON Database/task/src/server/Main.java
+++ JSON Database/task/src/server/Main.java
@@ -1,55 +1,36 @@
 package server;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static String[] array = new String[100];
+    private static final int port = 23456;
 
     public static void main(String[] args) {
-        Arrays.fill(array, "");
-        Scanner sc = new Scanner(System.in);
-        String command = "";
-        int numofcell = 0;
-        String text = "";
+        try {
+            ServerSocket server = new ServerSocket(port);
 
-        do {
-            System.out.print("> ");
-            command =  sc.next();
+            System.out.println("Server started!");
+            Socket client = server.accept();
 
-            switch (command) {
-                case("set"):
-                    numofcell = sc.nextInt() - 1;
-                    text = sc.nextLine();
-                    if (numofcell>=0 && numofcell<=99) {
-                        array[numofcell] = text;
-                        System.out.println("OK");
-                    }
-                    break;
-                case("get"):
-                    numofcell = sc.nextInt() - 1;
-                    if (numofcell>=0 && numofcell<=99 && !array[numofcell].isEmpty()) {
-                        System.out.println(array[numofcell]);
-                    }
-                    else{
-                        System.out.println("ERROR");
-                    }
-                    break;
-                case("delete"):
-                    numofcell = sc.nextInt() - 1;
-                    if (numofcell>=0 && numofcell<=99) {
-                        array[numofcell] = "";
-                        System.out.println("OK");
-                    }
-                    else {
-                        System.out.println("ERROR");
-                    }
-                    break;
-                default:
-                    break;
-            }
+            DataInputStream input = new DataInputStream(client.getInputStream());
+            DataOutputStream output = new DataOutputStream(client.getOutputStream());
+
+            String m = input.readUTF();
+            String m2 = input.readUTF();
+
+            System.out.println("Received: "+ m + m2);
+            output.writeUTF("A record # " + m2 + " was sent!");
+            System.out.println("Sent: " + "A record # " + m2 + " was sent!");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        while (!command.equals("exit"));
 
     }
 }
