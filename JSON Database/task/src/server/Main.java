6516647cc4f0381a48a2c311c1eef56602cf6155
KronosGR <geo.elgeo@gmail.com>
2022-12-01 11:08:06 +0100

    stage 3/6 server deved

--- JSON Database/task/src/server/Main.java
+++ JSON Database/task/src/server/Main.java
@@ -6,28 +6,62 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.ClientInfoStatus;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     private static final int port = 23456;
-
+    public  static String[] array = new String[1000];
     public static void main(String[] args) {
+        // fill array with empty string
+        Arrays.fill(array, "");
+
+        String text = "";
+        String type = "";
+        int index = 0;
+
         try {
             ServerSocket server = new ServerSocket(port);
-
             System.out.println("Server started!");
-            Socket client = server.accept();
+            while (true) {
+                Socket client = server.accept();
 
-            DataInputStream input = new DataInputStream(client.getInputStream());
-            DataOutputStream output = new DataOutputStream(client.getOutputStream());
+                DataInputStream input = new DataInputStream(client.getInputStream());
+                DataOutputStream output = new DataOutputStream(client.getOutputStream());
+                type = input.readUTF();
+                index = Integer.parseInt(input.readUTF()) -1;
 
-            String m = input.readUTF();
-            String m2 = input.readUTF();
+                if (type.equals("exit")){
+                    break;
+                }
 
-            System.out.println("Received: "+ m + m2);
-            output.writeUTF("A record # " + m2 + " was sent!");
-            System.out.println("Sent: " + "A record # " + m2 + " was sent!");
+                switch (type){
+                    case "set":
+                        text = input.readUTF();
+                        if (index >-1 && index <1000){
+                            array[index] = text;
+                            output.writeUTF("OK");
+                        }
+                        break;
+                    case "get":
+                        if (index >-1 && index < 100 && !array[index].isEmpty()){
+                            output.writeUTF(array[index]);
+                        } else {
+                            output.writeUTF("ERROR");
+                        }
+                        break;
+                    case "delete":
+                        if (index > -1 && index <100){
+                            array[index] = "";
+                            output.writeUTF("OK");
+                        } else {
+                            output.writeUTF("ERROR");
+                        }
+                        break;
+                }
+            }
+            server.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
